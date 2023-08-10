c40acba2c73a9f580b7452e834dad26a65aad0d5
Gaurav <83856198+GauravSamanta@users.noreply.github.com>
2023-08-10 21:25:06 +0530

    updated about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ function About() {
       </div>
       <div className="font-Inter text-[#808080] text-2xl ">
       Passionate problem solver and computer engineering student <br />
-      with a focus on game development and software engineering. <br />Committed to exceptional outcomes through coding expertise. </div>
+      with a focus on game development and software engineering. <br />Committed to exceptional outcomes through coding expertise. <br />My favourite element is Nickel </div>
       <div className="flex flex-row py-3.5 space-x-4 mt-5">
       <div className="basis-1/4 btn pr-px text-[#d9d9d9]"> <input type="button" value="My Resume" /></div>
       <div className="basis-1/4 btn text-[#d9d9d9]"> <input type="button" value="Contact Me" /></div>
